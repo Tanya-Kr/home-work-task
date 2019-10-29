05ac0f98de4a421934d25ee6b05275f84a3f6d6b
Naitiri <tanya_krugl@mail.ru>
2019-10-29 14:12:00 +0200

    1 task

--- src/main/java/com/hillel/lecture_4/FibonacciNumbersChecker.java
+++ src/main/java/com/hillel/lecture_4/FibonacciNumbersChecker.java
@@ -9,8 +9,20 @@ public class FibonacciNumbersChecker {
     public int[] getFibonacciNumbers(int number) {
 
 //        TODO implements result
-        int[] result = { };
+        int[] result = new int[number];
 
+        int n0 = 0;
+        int n1 = 1;
+        int n2 = 1;
+        int n3;
+        result[0] = n0;
+        result[1] = n1;
+        for (int i = 2; i < number; i++) {
+            n3 = n1 + n2;
+            result[i] = n2;
+            n1 = n2;
+            n2 = n3;
+        }
         return result;
     }
 
