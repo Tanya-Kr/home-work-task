b667a93abd3f1536253abebafda6a235bf20afa3
Tanya-Kr <tanya.krug7@gmail.com>
2019-10-30 20:32:04 +0200

    loop sort

--- src/test/java/com/hillel/lecture_4/ArrayOfArray.java
+++ src/test/java/com/hillel/lecture_4/ArrayOfArray.java
@@ -2,13 +2,30 @@ package com.hillel.lecture_4;
 
 public class ArrayOfArray {
     public static void main(String[] args) {
-//        int[][] array = new int[3][3];
-        int[][] array = {{1, 2, 6}, {5, 6,7}};
+////        int[][] array = new int[3][3];
+//        int[][] array = {{1, 2, 6}, {5, 6,7}};
+//
+//        int[] a = array[1];
+//        int b = array[1][1];
+//        System.out.println(a[1]);
+//        System.out.println(b);
+        int[] values = { 20, 4, 3, 10, 2, -5, 0, -13, 1 };
+        int[] result = new int[values.length];
 
-        int[] a = array[1];
-        int b = array[1][1];
-        System.out.println(a[1]);
-        System.out.println(b);
+        for(int i = result.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (values[j] > values[j + 1]) {
+                    int tmp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++){
+            result[i] = values[i];
+            System.out.printf(" " + result[i]);
+        }
     }
 
 }
